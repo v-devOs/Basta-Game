57230a04fe2ebf70844722e76b93889ce9bbd176
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-04-29 18:33:38 -0600

    fix(auth/register): Added styles and captcha function

--- app/auth/register.php
+++ app/auth/register.php
@@ -6,37 +6,14 @@ if (isset($_SESSION["role"])) {
 }
 session_destroy();
 
-function captcha(&$resu){
-   
-   // 3 Digitios y dos operadores
-   $opers = "+-*";
-   $ope1 = $opers[rand()%3]; 
-   $ope2 = $opers[rand()%3];
+@include '../../components/navbar.php';
+@include '../../class/acceso.php';
 
-   $digi1 = rand()%9+1;
-   $digi2 = rand()%9+1;
-   $digi3 = rand()%9+1;
+$acceso = new Acceso();
 
-   $resu = resuelve($digi1, $digi2, $ope1);
-   $resu = resuelve($resu, $digi3, $ope2);
-   $cap1 = $digi1.$ope1.$digi2.$ope2.$digi3;
-   
+$cap = $acceso->generarCaptcha('cap_register');
 
-   return $cap1;
-}
 
-function resuelve($dig1,$dig2,$ope){
-   if($ope=="+") return $dig1+$dig2;
-   else if($ope=='-') return $dig1-$dig2;
-   else return $dig1*$dig2;
-}
-
-$resulLogin = $resuRegistro = $resuContra = 0;
-$cap1 = captcha($resuRegistro);
-
-$_SESSION["capt_register"] = $resuRegistro;
-
-echo ($resuRegistro);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -45,29 +22,72 @@ echo ($resuRegistro);
    <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <title>Register</title>
+   <link rel="stylesheet" href="../css/styles.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  
 </head>
 
 <body>
-   <form action="../../class/acceso.php" method="POST">
-      <input type="hidden" name='action' value='register'>
-      <input type="text" name="nombre" placeholder="Nombre" />
-      <input type="text" name="apellidos" placeholder="Apellidos" />
-      <input type="text" name="correo" placeholder="mail@mail.com" />
 
-      <input type="text" name="capchat" placeholder="Cuanto es <?=$cap1;?>">
+   <div class="container mt-5">
+      <div class="justify-content-center row">
+         <div class="col-lg-6 col-sm-12">
+            
+            <h2 class="mb-4">Ingresa tus datos</h2>
 
-      <input type="password" name="clave" placeholder="Contraseña" />
-      <!-- <?php
-      if (isset($_GET['e'])) {
-         if ($_GET['e'] == 1) {
-            echo "Error en las credenciales";
-         } else if ($_GET['e'] == 2) {
-            echo "Los datos no han sido enviados";
-         }
-      }
-      ?> -->
-      <input type="submit" value="Ingresar" />
-   </form>
+            <form class="row g-3" action="../../class/acceso.php" method="post">
+
+               <input type="hidden" name='action' value='register'>
+
+               <div class="col-6">
+                  <label for="" class="form-label">Nombre/s</label>
+                  <input class="form-control" type="text" name="nombre" placeholder="Nombre" />
+               </div>
+               <div class="col-6">
+                  <label for="" class="form-label">Apellidos</label>
+                  <input class="form-control" type="text" name="apellidos" placeholder="Apellidos" />
+               </div>
+
+               <div class="col-12">
+                  <label for="" class="form-label">Correo</label>
+                  <input class="form-control" type="text" name="correo" placeholder="mail@mail.com" />
+               </div>
+
+               <div class="col-12 mb-4">
+                  <h4 >Selecciona tu genero</h4>
+
+                  <select class="form-select ">
+                     <option value="Otro">Otro</option>
+                     <option value="Hombre">Hombre</option>
+                     <option value="Mujer">Mujer</option>
+                  </select>
+               </div>
+
+               <div class="col-12 mb-4">
+                  <h4>Capchat</h4>
+                  <input class="form-control" type="text" name="capchat" placeholder="Cuanto es <?=$cap;?>">
+               </div>
+         
+               <!-- <?php
+               if (isset($_GET['e'])) {
+                  if ($_GET['e'] == 1) {
+                     echo "Error en las credenciales";
+                  } else if ($_GET['e'] == 2) {
+                     echo "Los datos no han sido enviados";
+                  }
+               }
+               ?> -->
+
+               <div class="col-4">
+                  <input class="btn btn-primary" type="submit" value="Ingresar" />
+               </div>
+            </form>
+
+         </div>
+      </div>
+   </div>
+
 </body>
 
 </html>
